58bf1a8c7a5157fc974f31f2018fd2a951b888f3
Valerii <valeri.github@gmail.com>
2024-01-20 18:31:09 +0300

    Порядок имеет значение

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ async def root():
     return {"message": "Hello world"}
 
 
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id):
+async def read_item(item_id: int):
     return {"item_id": item_id}
