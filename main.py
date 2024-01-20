8ba3c1f97bfce0ad24b0785a84815d5b2915fbc8
Valerii <valeri.github@gmail.com>
2024-01-20 18:33:55 +0300

    AlexNet, ResNet и LeNet - это названия моделей машинного обучения

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ async def root():
     return {"message": "Hello world"}
 
 
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
