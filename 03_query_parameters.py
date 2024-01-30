743918f2afcdd52de71b9ac6d10f6c8d20992e94
Valerii <valeri.github@gmail.com>
2024-01-30 15:10:33 +0300

    Add Multiple path and query parameters

--- 03_query_parameters.py
+++ 03_query_parameters.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -15,9 +15,38 @@ fake_items_db = [
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip + limit]
 
+
 # Необязательные параметры
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str = None):
+async def read_item(item_id: str, q: Union[str, None] = None):
     if q:
         return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
+
+
+# Преобразование типа параметра запроса
+@app.get("/bool_items/{bool_item_id}")
+async def read_item(bool_item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"bool_item_id": bool_item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# Смешивание query-параметров и path-параметров
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
