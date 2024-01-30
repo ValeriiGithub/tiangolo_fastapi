dc65db7b13d60dee11326cc6a7b301f9a13e858f
Valerii <valeri.github@gmail.com>
2024-01-30 16:52:28 +0300

    Тело запроса + параметры пути + параметры запроса

--- 04_Request_Body.py
+++ 04_Request_Body.py
@@ -17,3 +17,21 @@ app = FastAPI()
 @app.post("/items/")
 async def create_item(item: Item):
     return item
+
+# Использование модели
+@app.post("/items_use/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+# Тело запроса + параметры пути + параметры запроса
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
