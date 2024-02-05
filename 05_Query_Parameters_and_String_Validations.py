5bd00eff1d0c4f2b1833311b31552aa2382e0940
Valerii <valeri.github@gmail.com>
2024-02-05 17:54:56 +0300

    pydantic:Required has been removed in V2.

--- 05_Query_Parameters_and_String_Validations.py
+++ 05_Query_Parameters_and_String_Validations.py
@@ -1,5 +1,5 @@
-from typing import Union
-from pydantic import Required
+from typing import Union, List
+# from pydantic import Required # has been removed in V2.
 from fastapi import FastAPI, Query
 from typing_extensions import Annotated
 
@@ -7,8 +7,16 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[str, Query(min_length=3)] = Required):
+async def read_items(q: Annotated[str, Query(min_length=3)]):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
     return results
+
+
+# Множество значений для query-параметра
+@app.get("/items_2/")
+async def read_items(q: Annotated[Union[List[str], None], Query()] = None):
+    query_items = {"q": q}
+    return query_items
+
