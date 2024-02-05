bea7bfff9ad52094bb84ece6008de19530267197
Valerii <valeri.github@gmail.com>
2024-02-05 17:26:46 +0300

    Import Query and Annotated

--- 05_Query_Parameters_and_String_Validations.py
+++ 05_Query_Parameters_and_String_Validations.py
@@ -1,12 +1,13 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing_extensions import Annotated
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Union[str, None] = None):
+async def read_items(q: Annotated[Union[str, None], Query(max_length=50)] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
