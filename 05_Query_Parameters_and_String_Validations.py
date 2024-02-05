8fe189bd37d639b8ef1cbdc35285b6d1ac3e9632
Valerii <valeri.github@gmail.com>
2024-02-05 17:40:57 +0300

    Add regular expressions

--- 05_Query_Parameters_and_String_Validations.py
+++ 05_Query_Parameters_and_String_Validations.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[Union[str, None], Query(min_length=3, max_length=5)] = None):
+async def read_items(q: Annotated[Union[str, None], Query(min_length=3, max_length=5, pattern="^fixedquery$")] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
