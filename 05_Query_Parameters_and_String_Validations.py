488ab701371bba2319f261e8790fc4a04ef3990a
Valerii <valeri.github@gmail.com>
2024-02-05 18:15:10 +0300

    add title and discription

--- 05_Query_Parameters_and_String_Validations.py
+++ 05_Query_Parameters_and_String_Validations.py
@@ -20,3 +20,38 @@ async def read_items(q: Annotated[Union[List[str], None], Query()] = None):
     query_items = {"q": q}
     return query_items
 
+# Query-параметр со множеством значений по умолчанию
+@app.get("/items_3/")
+async def read_items(q: Annotated[List[str], Query()] = ["foo", "bar"]):
+    query_items = {"q": q}
+    return query_items
+
+# Больше метаданных¶
+#
+# Вы можете добавить больше информации об query-параметре.
+# Вы можете указать название query-параметра, используя параметр title:
+@app.get("/items_4/")
+async def read_items(
+    q: Annotated[Union[str, None], Query(title="Query string", min_length=3)] = None
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items_5/")
+async def read_items(
+    q: Annotated[
+        Union[str, None],
+        Query(
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
