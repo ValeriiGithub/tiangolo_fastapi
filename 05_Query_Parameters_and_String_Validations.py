6376862dbb22d58d6181fd68ef13ae5e6f2ef92c
Valerii <valeri.github@gmail.com>
2024-02-05 17:38:44 +0300

    Add min_length

--- 05_Query_Parameters_and_String_Validations.py
+++ 05_Query_Parameters_and_String_Validations.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[Union[str, None], Query(max_length=50)] = None):
+async def read_items(q: Annotated[Union[str, None], Query(min_length=3, max_length=5)] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
